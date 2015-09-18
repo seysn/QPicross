aa6309245e514d27f0f486ad44c11fb7a2ec9735
Nicolas Seys <seys.nicolas@gmail.com>
2015-09-18 17:49:45 +0200

    added two test levels

--- src/campaignmaps.h
+++ src/campaignmaps.h
@@ -1,5 +1,28 @@
 #ifndef CAMPAIGNMAPS
 #define CAMPAIGNMAPS
 
+#include <vector>
+#include <level.h>
+
+#define NB_LEVEL 1
+
+/*
+ * To "prevent" cheating, I placed campaign
+ * maps in this header instead of files to
+ * make them a bit hidden.
+ * I'll find a better way soon. Actually,
+ * it's just for testing.
+ */
+
+std::vector<std::vector<int> > level1 =
+        {{EMPTY , FILLED, EMPTY },
+         {FILLED, FILLED, FILLED},
+         {EMPTY , FILLED, EMPTY }};
+
+std::vector<std::vector<int> > level2 =
+        {{FILLED, FILLED, FILLED},
+         {FILLED, EMPTY , FILLED},
+         {FILLED, FILLED, FILLED}};
+
 #endif // CAMPAIGNMAPS
 
